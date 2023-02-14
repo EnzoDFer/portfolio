881beaceb074869c5d6d30e310c96948fdb87cdf
enzodfer <enzodfer@outlook.com>
2023-02-13 19:57:52 -0500

    Add test for Slide Add test to check correct color rendering when providing an hsl input

--- __tests__/Slide.test.tsx
+++ __tests__/Slide.test.tsx
@@ -3,12 +3,21 @@ import { Slide } from '@/components/presentational/Slide/Slide';
 
 describe(Slide, () => {
   it('renders section tag', () => {
-
     render(<Slide 
       id='test'
-      hsl='hsl(0,0,0,.6)'
+      hsl='hsl(0,0%,0%,.6)'
     />)
 
     expect(screen.getByTestId('test')).toBeInTheDocument();
-  })
+  });
+
+  it('colors element correctly', () => {
+    render(<Slide 
+      id='test'
+      hsl='hsl(0,0%,0%,.6)'
+    />)
+    const slide = screen.getByTestId('test');
+    const backgroundColor = window.getComputedStyle(slide).getPropertyValue('background-color');
+    expect(backgroundColor).toBe('rgba(0, 0, 0, 0.6)');
+  });
 })
